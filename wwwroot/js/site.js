01b04e70d427a63f48934a2579ccf82c10dd5fd1
amcmullen9291 <amcmullen9291@gmail.com>
2022-02-10 19:38:06 -0500

    onclick function() and DOM.appendChild from <script>

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function sayHello(e){
+    e.preventDefault();
+    alert("first onClick function");
+}
